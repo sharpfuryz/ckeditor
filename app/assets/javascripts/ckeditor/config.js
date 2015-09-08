cda9a55e5631435e94de46a457793cb603a46ee2
Dmytry <sharpfuryz@gmail.com>
2015-09-08 15:18:16 +0300

    Feature: Bootstrap skin

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -9,6 +9,7 @@ CKEDITOR.editorConfig = function( config )
   // config.language = 'fr';
   // config.uiColor = '#AADC6E';
   config.extraPlugins = 'toolbar,notification,autosave';
+  config.skin = 'bootstrapck';
   config.font_names = 'ProximaNova/ProximaNova;' + config.font_names; //Unreal awesome fix
   config.uiColor = '#2c303e';
   /* Filebrowser routes */
